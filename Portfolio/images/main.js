2e512c81b0216823ed7b7889bd96543cf21a1bba
Mounika-8436 <eslavath.mounika2023@gmail.com>
2025-05-11 21:43:34 -0400

    Add files via upload

---
+++ Portfolio/images/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Software Developer" , "Web Developer" , "Programmer Analyst"],
+    typeSpeed: 150,
+    backSpeed: 80,
+    backDelay: 1500,
+    loop: true,
+  });
+  
